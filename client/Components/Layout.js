237adcc73886a96cd1eb29273006ed12a864321b
Prathamesh47 <pratham03d@gmail.com>
2023-10-01 16:36:05 +0530

    gif added

--- client/Components/Layout.js
+++ client/Components/Layout.js
@@ -6,6 +6,7 @@ import Loader from "./Loader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as THREE from "three";
 import * as styles from "../styles/layout.module.css";
+import bat from "../public/bat_animation .gif"
 
 import headerImg from "../public/astronaut.svg";
 
@@ -470,28 +471,46 @@ const Layout = ({ children }) => {
       <Header />
       <div className={`flex-1 ${router.pathname === "/" ? "" : "background"}`}>
         {router.pathname !== "/" && (
-          <div
-            id="bg"
-            className="fixed bottom-0 left-0 right-0 min-w-full min-h-full"
-            style={{
-              zIndex: -2, // Add this line to move it further to the background
-              height: "100vh",
-              width: "100%",
-            }}
-          >
-            <img
-              id="bg_img"
-              style={{
-                zIndex: -1,
-                height: "100%",
-                width: "100%",
-                objectFit: "cover",
-              }}
-              src="main.png"
-              alt="Background Image"
-              // className={loader ? "false" : ""}
-            />
-          </div>
+           <div
+           id="bg"
+           className="fixed bottom-0 left-0 right-0 min-w-full min-h-full "
+           style={{
+             zIndex: -2,
+             height: "100vh",
+             width: "100%",
+           }}
+         >
+           {/* Background Image */}
+           <img
+             id="bg_img"
+             style={{
+               zIndex: -1,
+               position: "absolute", // Use absolute positioning
+               top: 0,
+               left: 0,
+               height: "100%",
+               width: "100%",
+               objectFit: "cover",
+             }}
+             src="main.png"
+             alt="Background Image"
+           />
+           {/* Overlay Image */}
+           <img
+             id="bg_img"
+             style={{
+               zIndex: 0, // You can adjust this value to control the stacking order
+               position: "absolute", // Use absolute positioning
+               top: 0,
+               left: 0,
+               height: "100%",
+               width: "100%",
+               objectFit: "cover",
+             }}
+             src="bat_animation .gif"
+             alt="Overlay Image"
+           />
+         </div>
         )}
 
         {children}
