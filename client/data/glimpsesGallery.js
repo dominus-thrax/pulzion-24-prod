6532ae3f5fa7d14f1b2df9d1c990b2c89a579d4d
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-10-05 23:49:10 +0530

    Merged Glimpses

--- client/data/glimpsesGallery.js
+++ client/data/glimpsesGallery.js
@@ -1,116 +1,116 @@
 export const images = {
-    "pulzion": [
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/1.jpeg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/1.jpeg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/10.png",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/10.png"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/14.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/14.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/20220424_200331.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/20220424_200331.jpg"
-      },
+  "pulzion": [
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01649.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01649.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01654.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01654.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01768.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01768.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01769.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01769.jpg"
+    },
     //   {
     //     "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/360_F_412572270_OGw5hFLVwWoBCOdwWjLa1qGHsYU444PI.jpg",
     //     "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/360_F_412572270_OGw5hFLVwWoBCOdwWjLa1qGHsYU444PI.jpg"
     //   },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/4.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/4.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/5.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/5.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/6.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/6.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/7.jpeg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/7.jpeg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/Copy of 1650600867022-01.jpeg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/Copy of 1650600867022-01.jpeg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/Copy of DSC_1272.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/Copy of DSC_1272.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/Copy of IMG_5208.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/Copy of IMG_5208.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/Copy of IMG_5220.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/Copy of IMG_5220.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC_1480.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC_1480.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC_1551.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC_1551.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC_1617-01.jpeg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC_1617-01.jpeg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC_1639-02.jpeg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC_1639-02.jpeg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_1485.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_1485.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_1515.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_1515.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_1534.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_1534.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_20220423_155516.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_20220423_155516.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_20220423_162144.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_20220423_162144.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_20220423_170217.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_20220423_170217.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_20220423_170303.jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_20220423_170303.jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_20220424_190503 (1).jpg",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_20220424_190503 (1).jpg"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5082.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5082.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5176.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5176.JPG"
-      },
-      {
-        "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5191.JPG",
-        "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5191.JPG"
-      }
-    ]
-  }
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01838.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01838.jpg"
+    },
+    // {
+    //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02064.jpg",
+    //   "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02064.jpg"
+    // },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02093.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02093.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02122.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02122.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02262.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02262.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02302.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02302.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02325.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02325.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02455.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02455.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02469.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02469.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02519.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02519.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02693.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02693.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02729.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02729.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02822.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02822.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02901.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02901.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02928.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02928.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03144.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC03144.jpg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03176.jpg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC03176.jpg"
+    },
+    // {
+    //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03194.jpg",
+    //   "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC03194.jpg"
+    // },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/WhatsApp Image 2023-06-15 at 1.36.32 PM (1).jpeg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/WhatsApp Image 2023-06-15 at 1.36.32 PM (1).jpeg"
+    },
+    {
+      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/WhatsApp Image 2023-06-15 at 1.36.33 PM.jpeg",
+      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/WhatsApp Image 2023-06-15 at 1.36.33 PM.jpeg"
+    },
+    // {
+    //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5082.JPG",
+    //   "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5082.JPG"
+    // },
+    // {
+    //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5176.JPG",
+    //   "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5176.JPG"
+    // },
+    // {
+    //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5191.JPG",
+    //   "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/IMG_5191.JPG"
+    // }
+  ]
+}
