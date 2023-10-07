ed1fd592e831941cb36d64c58f936c2dc45914c9
dominus-thrax <146502689+dominus-thrax@users.noreply.github.com>
2023-10-07 12:33:59 +0530

    Update [id].js

--- client/pages/event/[id].js
+++ client/pages/event/[id].js
@@ -1,57 +1,23 @@
-import Layout from '../../Components/Layout';
-import { useRouter } from 'next/router'
-import EventDetails from '../../Components/EventDetails';
-import { getEvent } from '../../action/registeration';
+import Layout from "../../Components/Layout";
+import { useRouter } from "next/router";
+import EventDetails from "../../Components/EventDetails";
+import { getEvent } from "../../action/registeration";
+import { useEffect, useState } from "react";
 
-const EventDetailsPage = ({ event }) => {
+const EventDetailsPage = () => {
+	const [event, setEvent] = useState(null);
+	const router = useRouter();
+	useEffect(() => {
+		let { id } = router.query;
+		if (id) {
+			(async () => {
+				let res = await getEvent(id);
+				setEvent(res.events);
+			})();
+		}
+	}, [router]);
 
-    const router = useRouter();
-    let { id } = router.query;
+	return <Layout>{!event ? "" : <EventDetails event={event} />}</Layout>;
+};
 
-    console.log("Event: ")
-    console.log(event)
-
-
-    return (
-        <Layout>
-
-            {
-                !event ? '' :
-                    <EventDetails event={event} />
-            }
-        </Layout >
-
-    )
-}
-
-export default EventDetailsPage
-
-
-export async function getServerSideProps(context) {
-    const { id } = context.query;  //grab the id from the url
-    let event = null;
-    try {
-        console.log("API URL: ");
-
-        await getEvent(id).then(res => {
-            console.log("Response: ");
-            console.log(res);
-            event = res.events;
-            console.log(event);
-        }).catch(err => {
-            console.log(err);
-
-        })
-
-    } catch (err) {
-        console.log("Error: ");
-        console.log(err);
-        event = err;
-    }
-
-    return {
-        props: {
-            event,
-        }
-    }
-}
+export default EventDetailsPage;
