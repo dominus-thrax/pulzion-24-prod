5e054f93f31d131e9699884d7c5e97005b80915d
parthasawa100 <parthasawa100@gmail.com>
2023-10-11 16:59:18 +0530

    glimpses added firebase

--- client/data/glimpsesGallery.js
+++ client/data/glimpsesGallery.js
@@ -1,20 +1,20 @@
 export const images = {
   "pulzion": [
     {
-      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01649.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01649.jpg"
+      "original": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Foriginal%2FDSC01649.JPG?alt=media&token=5c797b14-2b29-49d9-87f8-3f8f44c996a0&_gl=1*td09u2*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAxNjc1Ny4xLjEuMTY5NzAxODMyMy42MC4wLjA.",
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01649.jpg?alt=media&token=c460bdd4-9c65-4de7-b74b-8a77d891757b&_gl=1*1uvny8u*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAxNjc1Ny4xLjEuMTY5NzAxODM4Ni41Ny4wLjA."
     },
     {
-      "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01654.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01654.jpg"
+      "original": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01654.jpg?alt=media&token=aaf70e59-2333-4d4b-98f9-b057d9601097&_gl=1*1201ooz*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMjk2OC4xNS4wLjA.",
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01654.jpg?alt=media&token=aaf70e59-2333-4d4b-98f9-b057d9601097&_gl=1*1te2cd0*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzAzNS4xMS4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01768.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01768.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01768.jpg?alt=media&token=190c57e5-dd05-4a2b-9c96-24f4646c4ff0&_gl=1*156r28u*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzA4Ny42MC4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01769.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01769.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01769.jpg?alt=media&token=1bb3e07f-6700-45ff-8ba7-15afa3198d53&_gl=1*nq6450*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzEwMS40Ni4wLjA."
     },
     //   {
     //     "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/360_F_412572270_OGw5hFLVwWoBCOdwWjLa1qGHsYU444PI.jpg",
@@ -22,7 +22,7 @@ export const images = {
     //   },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC01838.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC01838.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC01838.jpg?alt=media&token=82eb60a6-6ff7-4408-9ee8-0d074efb24d7&_gl=1*ikgppx*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzEzOC45LjAuMA.."
     },
     // {
     //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02064.jpg",
@@ -30,63 +30,63 @@ export const images = {
     // },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02093.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02093.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02093.jpg?alt=media&token=2f9b2fa0-01f8-4a0a-935c-7008a31c7874&_gl=1*hslvyi*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzE4My4yNy4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02122.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02122.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02122.jpg?alt=media&token=ff147177-4b9d-4ab4-88f3-65cd0c8c204f&_gl=1*7zpdp6*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzE5OC4xMi4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02262.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02262.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02262.jpg?alt=media&token=dfef42c0-63f1-49c9-8bda-75da83cd1997&_gl=1*qo402j*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzIyNy42MC4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02302.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02302.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02302.jpg?alt=media&token=f4faef4c-2a99-4b67-85ac-55ec2c6ed7c3&_gl=1*kvaxah*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzI0MC40Ny4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02325.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02325.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02325.jpg?alt=media&token=2333d4af-a966-4f23-b236-eb0c8bc2c0e1&_gl=1*h12p50*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzI2MC4yNy4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02455.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02455.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02455.jpg?alt=media&token=17ceafc7-69e4-43bc-bdeb-38621551a0d2&_gl=1*dug0g*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzI3Ni4xMS4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02469.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02469.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02469.jpg?alt=media&token=94e988df-e0b2-4bb4-82c7-f2bf2a54b742&_gl=1*vr7zty*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzI5OC42MC4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02519.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02519.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02519.jpg?alt=media&token=55730212-f449-408b-94ad-f457f1225963&_gl=1*11ebh06*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzMxNS40My4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02693.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02693.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02693.jpg?alt=media&token=4b765e78-1334-4be1-8107-8db94c26cdbd&_gl=1*1cr4hkl*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzMzMC4yOC4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02729.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02729.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02729.jpg?alt=media&token=15526f69-9ae4-4b43-91f1-ea3f86e88362&_gl=1*1ajgqn4*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzM0Ny4xMS4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02822.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02822.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02822.jpg?alt=media&token=2d12f84c-9b5c-4e5e-9d42-aad9a39332e3&_gl=1*13var3j*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzM2NS42MC4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02901.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02901.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02901.jpg?alt=media&token=cca06a59-fb77-4ae7-bc90-24e27b92adaa&_gl=1*1bwyo8b*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzM4NC40MS4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC02928.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC02928.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC02928.jpg?alt=media&token=199339b1-af88-4994-aa5c-dc00ea6106cf&_gl=1*1t65mw2*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzQwMi4yMy4wLjA."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03144.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC03144.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC03144.jpg?alt=media&token=7fa444b7-413e-43f0-8039-642d98c4cc74&_gl=1*1u4p6sv*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzQxNy44LjAuMA.."
     },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03176.jpg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/DSC03176.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FDSC03176.jpg?alt=media&token=317e7feb-19c3-4bc5-a37a-9ce78de1ca9b&_gl=1*12fqlwq*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzQzNi42MC4wLjA."
     },
     // {
     //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/DSC03194.jpg",
@@ -98,7 +98,7 @@ export const images = {
     // },
     {
       "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/WhatsApp Image 2023-06-15 at 1.36.33 PM.jpeg",
-      "thumbnail": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/thumbnail/WhatsApp Image 2023-06-15 at 1.36.33 PM.jpg"
+      "thumbnail": "https://firebasestorage.googleapis.com/v0/b/pasc-b1685.appspot.com/o/pulzion-ems%2Fthumbnails%2FWhatsApp%20Image%202023-06-15%20at%201.36.33%20PM.jpg?alt=media&token=e37791f9-e844-4ece-9901-da68413a72bc&_gl=1*68xhwn*_ga*MTE5MTk1MTYxNS4xNjk3MDE2NzU3*_ga_CW55HF8NVT*MTY5NzAyMjkyMy4yLjEuMTY5NzAyMzQ2Mi4zNC4wLjA."
     },
     // {
     //   "original": "https://pulzion-ems.s3.ap-south-1.amazonaws.com/original/IMG_5082.JPG",
